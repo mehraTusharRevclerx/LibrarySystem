14725ff3d0436c85c700e43a470caedb7ff2e51f
Tushar Mehra <tusharmehrarevclerx@gmail.com>
2022-06-23 22:58:55 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystem
+{
+    class Program
+    {
+        static void Main()
+        {
+            library<string> lib = new library<string>();
+
+            Console.WriteLine("Welcome To information center");
+            int option;
+            do
+            {
+                Console.WriteLine("\nPlease Select Option Down Below:\n1.Add Book\n2.Remove Book\n3.Total No of Books\n4.Display All Books\n5.Cancel");
+                string options = Console.ReadLine();
+                while (!int.TryParse(options, out option))
+                {
+                    options = Console.ReadLine();
+                }
+
+
+                switch (option)
+                {
+                    case 1:
+                        AddBook(lib);
+                        break;
+                    case 2:
+                        RemoveBook(lib);
+                        break;
+                    case 3:
+                        TotalNoOfBooks(lib);
+                        break;
+                    case 4:
+                        lib.DisplayAllBooks();
+                        break;
+                    case 5:
+                        Cancel();
+                        break;
+                    default:
+                        Console.WriteLine("Please Enter Correct Option");
+                        break;
+                }
+            } while (option != 5);
+
+
+            Console.ReadKey();
+
+        }
+        static void RemoveBook(library<string> lib)
+        {
+            lib.DisplayAllBooks();
+
+            lib.RemoveBookByIndex();
+
+        }
+        static void Cancel()
+        {
+            Console.WriteLine("Thanks For Visiting information center Please Visit Again\nPress Any Key To Exit");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+        static void AddBook(library<string> lib)
+        {
+            Console.WriteLine("Please Enter Your Book Name: ");
+            string currBook = Console.ReadLine();
+            lib.Add(currBook);
+            Console.WriteLine("Your Book Was Added\n");
+        }
+        static void TotalNoOfBooks(library<string> lib)
+        {
+            lib.TotalBooksCount();
+        }
+    }
+}
---
+++ library.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystem
+{
+    class library<T>
+    {
+        List<T> Books = new List<T>();
+
+
+        public void Add(T item)
+        {
+            Books.Add(item);
+            //if (count + 1 < Books.Count)
+            //{
+            //};
+
+        }
+        public T this[int index]
+        {
+            get { return this[index]; }
+            set { this[index] = value; }
+        }
+        public void TotalBooksCount()
+        {
+            Console.WriteLine($"Total Book Count in Library is {Books.Count}");
+        }
+
+        public void DisplayAllBooks()
+        {
+            if (Books.Count <= 0)
+            {
+                Console.WriteLine("Library Does Not Contains Any Book Sorry :(");
+                return;
+            }
+            else
+            {
+                Console.WriteLine($"\nAll Books Are Listed Down Below\n");
+                int count = 1;
+                foreach (var i in Books)
+                {
+                    Console.WriteLine($"{count}. {i}");
+                    count++;
+                }
+
+
+            }
+        }
+
+        public void RemoveBookByIndex()
+        {
+
+            if (Books.Count <= 0)
+            {
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Which Book You Want To Remove");
+                string bookIndexs = Console.ReadLine();
+                int bookIndex;
+                while (!int.TryParse(bookIndexs, out bookIndex))
+                {
+                    bookIndexs = Console.ReadLine();
+                }
+                Books.RemoveAt(bookIndex - 1);
+                Console.WriteLine("Your Book Was Removed SuccessFully");
+            }
+        }
+
+    }
+}
